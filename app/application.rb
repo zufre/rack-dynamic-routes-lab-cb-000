946db2a8e3d40e9ba176791e51928db2d9d211d7
Cedric David <cedric.david@btopenworld.com>
2018-09-18 05:24:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,9 +6,10 @@ class Application
     req = Rack::Response.new(env)
 
     if req.path.match(/items/)
-      item_name = req.path.split("/items").last
-      if item_name = @@items.find{|i| i.name.downcase == item_name.downcase}
-        resp.write item.price
+  item_name = req.path.split("/items/").last
+  if @@items.find {|i| i.name == item_name}
+    item = @@items.find {|i| i.name == item_name}
+    resp.write item.price
       else
         rep.write "Item not found"
         resp.status = 400
