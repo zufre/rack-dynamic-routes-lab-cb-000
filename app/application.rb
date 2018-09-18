df939188a00ffce9196eb8a8bad729ceddd22788
Cedric David <cedric.david@btopenworld.com>
2018-09-18 05:24:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,23 +1,24 @@
 class Application
-  @@items = []
 
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Response.new(env)
+    req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-  item_name = req.path.split("/items/").last
-  if @@items.find {|i| i.name == item_name}
-    item = @@items.find {|i| i.name == item_name}
-    resp.write item.price
-      else
-        rep.write "Item not found"
-        resp.status = 400
-      end
+      item_name = req.path.split("/items/").last
+      if @@items.find {|i| i.name == item_name}
+        item = @@items.find {|i| i.name == item_name}
+        resp.write item.price
+    else
+      resp.write "Item not found"
+      resp.status = 400
+    end
     else
       resp.write "Route not found"
       resp.status = 404
     end
     resp.finish
   end
+
+
 end
