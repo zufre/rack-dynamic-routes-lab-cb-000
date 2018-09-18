681164460f88a18b44b9ee82ff6e82c5ff152589
Cedric David <cedric.david@btopenworld.com>
2018-09-18 05:21:46 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,8 @@ class Application
     req = Rack::Response.new(env)
 
     if req.path.match(/items/)
-      item_name = req.path.split("/items").last
-      if item_name = @@items.find{|i| i.name.downcase == item_name.downcase}
+      item = req.path.split("/items").last
+      if item = @@items.find{|i| i.name.downcase == item_name.downcase}
         resp.write item.price
       else
         rep.write "Item not found"
