2e8cca752f3e27d7e6ddbf8c6003037096817fb8
Cedric David <cedric.david@btopenworld.com>
2018-09-18 05:20:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,20 @@
 class Application
-  
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Response.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items").last
+      if item = @@items.find{|i| i.name.downcase == item_name.downcase}
+        resp.write item.priceelse
+        rep.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
 end
