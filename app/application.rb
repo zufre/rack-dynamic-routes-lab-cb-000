acec46aec5abea48a4b3a940ee9895f5df5eabeb
Cedric David <cedric.david@btopenworld.com>
2018-09-18 05:31:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find {|i| i.name == item_name}
-      if !!item
-        
+      if item    
         resp.write item.price
     else
       resp.write "Item not found"
